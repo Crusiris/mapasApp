283947e73647f1e6ff4d0e562c2e364c66cd947f
crusiris <cruzsirys@gmail.com>
2019-07-09 12:18:37 -0400

    Componente mapa y instalacion de Angular material

---
+++ src/app/material.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+//Angular Material
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatButtonModule
+  ],
+  exports:[
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule
+  ]
+})
+export class MaterialModule { }
